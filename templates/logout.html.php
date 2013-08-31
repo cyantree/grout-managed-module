b4cc20b5700c10289bd7ca239cff328bb1503ee1
cyantree <mail@cyantree.com>
2013-08-31 11:57:29 +0200

    logout.html.php. Redirect to index after 3 seconds.

--- templates/logout.html.php
+++ templates/logout.html.php
@@ -4,7 +4,12 @@
 use Grout\ManagedModule\ManagedFactory;
 use Cyantree\Grout\App\Generators\Template\TemplateContext;
 
-$ui = ManagedFactory::get($this->app)->appUi();
-$q = ManagedFactory::get($this->app)->appQuick();
+$f = ManagedFactory::get($this->app);
+$ui = $f->appUi();
+$q = $f->appQuick();
 
 echo $ui->statusSuccess($q->t('Sie wurden erfolgreich abgemeldet.'));
+?>
+<script>
+    $.ct.redirect("<?=$q->e($f->appModule()->getUrl(), 'js')?>", 3);
+</script>
