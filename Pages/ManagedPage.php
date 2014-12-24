690348772352287397bfd6244ca1055797e89ff8
cyantree <mail@cyantree.com>
2014-12-24 03:09:14 +0100

    !! ManagedPage: Decoupled from AppModule. Optimized base template handling.

--- Pages/ManagedPage.php
+++ Pages/ManagedPage.php
@@ -4,13 +4,15 @@ namespace Grout\Cyantree\ManagedModule\Pages;
 use Cyantree\Grout\App\Page;
 use Cyantree\Grout\App\Types\ContentType;
 use Cyantree\Grout\App\Types\ResponseCode;
-use Grout\AppModule\Pages\TemplatePage;
+use Cyantree\Grout\Filter\ArrayFilter;
 use Grout\Cyantree\ManagedModule\ManagedFactory;
 
-class ManagedPage extends TemplatePage
+class ManagedPage extends Page
 {
     /** @var ManagedFactory */
     private $factory;
+
+    public $baseTemplate = 'CyantreeManagedModule:base.html';
 
     /** @return ManagedFactory */
     public function factory()
@@ -42,16 +44,30 @@ class ManagedPage extends TemplatePage
         parent::parseError($code, $data);
     }
 
-    public function setResult($content, $contentType = null, $responseCode = null, $baseTemplate = null)
+    public function parseTask()
     {
-        if ($baseTemplate !== false) {
-            $content = $this->factory()->templates()->load(
-                $baseTemplate === null ? 'CyantreeManagedModule:base.html' : $baseTemplate,
-                array('content' => $content),
-                false
-            )->content;
+        $this->setTemplateResult($this->task->vars->get('template'), $this->task->vars->get('templateData'), array(
+                'baseTemplate' => $this->task->vars->get('baseTemplate'),
+                'contentType' => $this->task->vars->get('contentType'),
+                'responseCode' => $this->task->vars->get('responseCode')
+        ));
+    }
+
+    public function setTemplateResult($template, $templateData = null, $settings = null)
+    {
+        $settings = new ArrayFilter($settings);
+
+        $content = $this->factory()->templates()->load($template, $templateData)->content;
+
+        $baseTemplate = $settings->get('baseTemplate');
+        if (!$baseTemplate && $baseTemplate !== false) {
+            $baseTemplate = $this->baseTemplate;
         }
 
-        parent::setResult($content, $contentType, $responseCode);
+        if ($baseTemplate) {
+            $content = $this->factory()->templates()->load($baseTemplate, array('content' => $content))->content;
+        }
+
+        $this->setResult($content, $settings->get('contentType'), $settings->get('responseCode'));
     }
 }
