c8547962e8acb229f7db1874a955633812e77ef4
cyantree <mail@cyantree.com>
2015-02-15 18:37:17 +0100

    * ManagedPlugin: Added type hint for $module.

--- Types/ManagedPlugin.php
+++ Types/ManagedPlugin.php
@@ -3,10 +3,14 @@ namespace Grout\Cyantree\ManagedModule\Types;
 
 use Cyantree\Grout\App\Plugin;
 use Grout\Cyantree\ManagedModule\ManagedFactory;
+use Grout\Cyantree\ManagedModule\ManagedModule;
 
 class ManagedPlugin extends Plugin
 {
     public $extendsService = false;
+
+    /** @var ManagedModule */
+    public $module;
 
     /** @var ManagedFactory */
     private $factory;
